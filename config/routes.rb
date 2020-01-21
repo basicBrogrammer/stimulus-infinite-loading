df59c4044ea9b56f7f475fdff064a402d574b2e7
Jeremy Ward <jeremy.ward@mckesson.com>
2020-01-21 09:03:10 -0500

    seed items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  root to: 'items#index'
 end
